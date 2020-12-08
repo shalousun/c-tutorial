d1cf7b00832c9d5cbb553f085a3d31fceb037ee7
oppofind <836575280@qq.com>
2020-12-09 01:02:27 +0800

    添加C语言hello world

---
+++ simple/HelloWorld.h
@@ -0,0 +1,8 @@
+//
+// Created by yu on 2020/12/9.
+//
+
+#ifndef C_TUTORIAL_HELLOWORLD_H
+#define C_TUTORIAL_HELLOWORLD_H
+
+#endif //C_TUTORIAL_HELLOWORLD_H
---
+++ simple/main.c
@@ -0,0 +1,11 @@
+//
+// Created by yu on 2020/12/9.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    return 0;
+}
